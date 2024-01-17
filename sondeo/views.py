cc8f7e1697e5c495ea948d0ae5d77f61dd5ff64e
dPenedo <dpenedo@tutanota.com>
2024-01-16 21:02:04 -0300

    avances en la clase 3 hasta Use the template system

--- sondeo/views.py
+++ sondeo/views.py
@@ -1,19 +1,20 @@
-from django.http import HttpResponse
-from django.template import loader
 from .models import Pregunta
+from django.shortcuts import render
+
 
 
 
 def index(request):
     lista_ultimas_preguntas = Pregunta.objects.order_by("-fecha_de_publicacion")[:5]
-    template = loader.get_template("sondeo/index.html")
     context = {
         "lista_ultimas_preguntas": lista_ultimas_preguntas,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "sondeo/index.html", context)
 
 def detalle(request, pregunta_id):
-    return HttpResponse("Estás viendo la pregunta %s." % pregunta_id)
+    pregunta = get_object_or_404(Pregunta, pk=pregunta_id) 
+    return render(request, "sondeo/detalle.html", {"pregunta": pregunta})
+
 
 def resultados(request, pregunta_id):
     respuesta = "Estás viendo los resultados de la pregunta %s "
